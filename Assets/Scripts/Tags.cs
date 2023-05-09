83feac72a43cde500d94b06b6d0dca307548178c
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2023-05-09 14:11:37 +0300

    Added Tags static class

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,10 @@
+/**
+ * Created Date: 5/9/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+public static class Tags {
+	public static string SHOP_TAG = "ShopKeeper";
+}
