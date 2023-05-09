66c51e9d79c1c9fb0dee85d321803eba9bb409d4
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2023-05-09 16:19:18 +0300

    Added methods to update gold amounts

--- Assets/Scripts/UI/Currency/CurrencyPresenter.cs
+++ Assets/Scripts/UI/Currency/CurrencyPresenter.cs
@@ -7,18 +7,44 @@
 
 namespace UI.Currency {
 	public sealed class CurrencyPresenter {
-		public int CurrencyAmount => this._model.CurrentGold;
+		public int GoldAmount => this._model.CurrentGold;
+		
 		private readonly CurrencyView _view;
 		private readonly CurrencyModel _model;
+		
+		#region Constructor
 
 		public CurrencyPresenter(CurrencyView view) {
 			this._view = view;
-			this._model = new CurrencyModel();
+			this._model = new CurrencyModel {
+				CurrentGold = UserSettings.Instance.Gold
+			};
+			
+			this.UpdateInitalCurrencyAmount();
 		}
+		
+		#endregion
+		
+		#region Public
 
-		public void UpdateCurrencyAmount(int amount) {
-			this._model.UpdateCurrency(amount);
-			this._view.UpdateCurrencyAmount(this._model.CurrentGold);
+		/// <summary>
+		/// Add or substract gold.
+		/// </summary>
+		/// <param name="amount">The amount of gold to be added or substracted.</param>
+		public void UpdateGoldAmount(int amount) {
+			this._model.UpdateGold(amount);
+			this._view.UpdateGoldAmount(this._model.CurrentGold);
+			UserSettings.Instance.SaveSettings();
 		}
+		
+		#endregion
+		
+		#region Private
+
+		private void UpdateInitalCurrencyAmount() {
+			this._view.UpdateGoldAmount(this._model.CurrentGold);
+		}
+		
+		#endregion
 	}
 }
