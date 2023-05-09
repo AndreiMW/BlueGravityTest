620ed283f1a130418c290e11b6619f4d75be9011
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2023-05-09 15:13:16 +0300

    Added ShopItem scriptable object

---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,19 @@
+/**
+ * Created Date: 5/9/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UnityEngine;
+
+namespace Shop {
+	[CreateAssetMenu(fileName = "Item", menuName = "Shop/Item", order = 1)]
+	public class ShopItem : ScriptableObject {
+		[field:SerializeField]
+		public int Price { get; private set; }
+		
+		[field:SerializeField]
+		public Color32 Color { get; private set; }
+	}
+}
