2d954d107711efafe9a0d26d5de2dce87d65a8f6
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2023-05-10 11:33:11 +0300

    Added logic to set button listener

--- Assets/Scripts/UI/Buttons/InventoryItemButton.cs
+++ Assets/Scripts/UI/Buttons/InventoryItemButton.cs
@@ -16,12 +16,37 @@ namespace UI.Buttons {
 		private Image _color;
 
 		private int _id;
+		private int _price;
+		private bool _shouldSell;
+
+		private InventoryItem _data;
 
 		#region Init
 
-		public void Init(ShopItemData inventoryData) {
-			this._id = inventoryData.Id;
-			this._color.color = inventoryData.Color;
+		public void Init(InventoryItem inventory) {
+			this._id = inventory.Id;
+			this._color.color = inventory.Color;
+			this._price = inventory.Price;
+			this._data = inventory;
+			
+			this.SetButtonListener();
+		}
+		
+		#endregion
+		
+		#region Private
+		
+		private void SetButtonListener() {
+			this.SetListener(Listener);
+			void Listener() {
+				if (UIManager.Instance.InventoryView.ShouldSellItems) {
+					InventoryManager.Instance.RemoveFromInventory(this._data);
+					UIManager.Instance.CurrencyPresenter.UpdateGoldAmount(this._price);
+					Destroy(this.gameObject);
+				} else {
+					Debug.Log("Equip");
+				}
+			}
 		}
 		
 		#endregion
