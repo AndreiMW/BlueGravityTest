8b8c71745b8e339a2fa2fe9b57f33df627cac6aa
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2023-05-10 10:10:25 +0300

    Added BuyOrSellView

---
+++ Assets/Scripts/UI/Views/BuyOrSellView.cs
@@ -0,0 +1,47 @@
+/**
+ * Created Date: 5/10/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using System;
+
+using UnityEngine;
+
+namespace Views {
+	public sealed class BuyOrSellView : BaseUIElement {
+		[SerializeField]
+		private BaseButton _buyButton;
+		
+		[SerializeField]
+		private BaseButton _sellButton;
+
+		public event Action OnBuyPressed;
+		public event Action OnSellPressed;
+
+		#region Lifecycle
+
+		private void Awake() {
+			this._buyButton.SetListener(this.HandleBuyPressed);
+			this._sellButton.SetListener(this.HandleSellPressed);
+		}
+
+		#endregion
+		
+		#region Private
+
+		private void HandleBuyPressed() {
+			this.Hide(0.2f);
+			this.OnBuyPressed?.Invoke();
+		}
+		
+		private void HandleSellPressed() {
+			this.Hide(0.2f);
+			this.OnSellPressed?.Invoke();
+		}
+		
+		#endregion
+
+	}
+}
